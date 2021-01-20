cb6202cb27b6380acd8120adfa78140bad814109
ramjirathore <ramjirathore2802@gmail.com>
2021-01-21 01:50:04 +0530

    simple blockchain but no verification yet

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(index, timestamp, data, previousHash) {
-        
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
     }
-}
+    
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+};
+
+class BlockChain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+       return new Block(0, new Date(), 'Genesis Block', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock); 
+    }
+
+};
+
+let minimalChain = new BlockChain();
+minimalChain.addBlock(new Block(1, new Date(), {amount: 379}));
+minimalChain.addBlock(new Block(2, new Date(), {amount: 1000}));
+
+console.log(JSON.stringify(minimalChain, null, 4));
