2b67d43303ce4407088328273949969ac4e5184b
ramjirathore <ramjirathore2802@gmail.com>
2021-03-14 22:25:15 +0530

    tut3 proofofwork and hashblock

--- blockchain.js
+++ blockchain.js
@@ -3,6 +3,8 @@ const sha256 = require('sha256');
 function Blockchain() {
 	this.chain = [];
 	this.pendingTransactions = [];
+	[];
+	this.createNewBlock(100, '0', '0');
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, Hash) {
@@ -40,6 +42,17 @@ Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, 
 	const hash = sha256(dataAsString);
 
 	return hash;
+};
+
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+	let nonce = 0;
+	let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	while (hash.substring(0, 4) !== '0000') {
+		nonce++;
+		hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+		console.log(hash);
+	}
+	return nonce;
 };
 
 module.exports = Blockchain;
--- test.js
+++ test.js
@@ -3,19 +3,45 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(3, 'EIUF44348927359HK', 'K89834HLK3543JSFJK');
+const previousBlockHash = 'KABH6788BJKJLH89897';
 
-bitcoin.createNewTransaction(300, 'aaa111', 'bbb111');
+const currentBlockData = [
+	{
 
-bitcoin.createNewBlock(3564, 'EIUF44348927359HK', 'K89834HLK3543JSFJK');
+		amount: 10,
+		sender: 'sdkjjns344',
+		recepeint: 'sdkjnsk3983'
+	},
+	{
+		amount: 20,
+		sender: 'sdkjjns344',
+		recepeint: 'sdkjnsk3983'
+	},
+	{
+		amount: 30,
+		sender: 'sdkjjns344',
+		recepeint: 'sdkjnsk3983'
+	}
+];
+const nonce = 100;
 
-bitcoin.createNewTransaction(300, 'ccc111', 'ddd111');
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
 
-bitcoin.createNewTransaction(400, 'eee111', 'fff111');
+// bitcoin.createNewBlock(3, 'EIUF44348927359HK', 'K89834HLK3543JSFJK');
 
-bitcoin.createNewTransaction(500, 'ggg111', 'hhh111');
+// bitcoin.createNewTransaction(300, 'aaa111', 'bbb111');
+
+// bitcoin.createNewBlock(3564, 'EIUF44348927359HK', 'K89834HLK3543JSFJK');
+
+// bitcoin.createNewTransaction(300, 'ccc111', 'ddd111');
+
+// bitcoin.createNewTransaction(400, 'eee111', 'fff111');
+
+// bitcoin.createNewTransaction(500, 'ggg111', 'hhh111');
 
 
-console.log(bitcoin);
-console.log(bitcoin.chain[0]);
+// console.log(bitcoin);
+// console.log(bitcoin.chain[0]);
 
